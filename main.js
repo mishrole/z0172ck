b7e6b551aa5b2efd7acf640e0dbf450e73af64dc
Erick172 <alejod172@gmail.com>
2020-05-28 15:34:57 -0500

    new version

---
+++ main.js
@@ -0,0 +1,10 @@
+const signupForm = document.querySelector('#signup-from');
+
+signupForm.addEventListener('subit', (e) => {
+    e.preventDefault();
+
+    const signupEmail = document.querySelector('#signup-form');
+    const signup = document.querySelector('#signup-form');
+
+    console.log('submiting')
+})
